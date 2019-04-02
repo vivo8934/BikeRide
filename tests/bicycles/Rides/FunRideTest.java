850982fc66ebbf6b0d4cac4353f177086b474e6d
Mfundo Ngqanekana <mfundo8934@gmail.com>
2019-04-02 11:08:31 +0200

    expanding funride

--- tests/bicycles/Rides/FunRideTest.java
+++ tests/bicycles/Rides/FunRideTest.java
@@ -9,29 +9,70 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FunRideTest {
 
+
     @Test
-    public void FunRides() {
-        FunRide bikeMax = new FunRide(8);
-        BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3, BicycleType.MountainBike);
-        BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4, BicycleType.RoadBike);
-        BicycleSpecification tandemspec = new BicycleSpecification(12, 7, BicycleType.Tandem);
+    public void CountForType() {
+        FunRide bikeMax = new FunRide(4);
+            BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3, BicycleType.MountainBike);
+            BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4, BicycleType.RoadBike);
+            BicycleSpecification tandemspec = new BicycleSpecification(12, 7, BicycleType.Tandem);
 
 
-        Bicycle rBike1 = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
-        Bicycle rBike = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
-        Bicycle mRide = new bicycles.specifications.BicycleFromSpec(mountainBikeSpec);
-        Bicycle tRide = new bicycles.specifications.BicycleFromSpec(tandemspec);
+            Bicycle rBike1 = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle rBike = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle mRide = new bicycles.specifications.BicycleFromSpec(mountainBikeSpec);
+            Bicycle tRide = new bicycles.specifications.BicycleFromSpec(tandemspec);
 
-        bikeMax.accept(rBike);
-        bikeMax.accept(rBike1);
-        bikeMax.accept(mRide);
-        bikeMax.accept(tRide);
+            bikeMax.accept(rBike);
+            bikeMax.accept(rBike1);
+            bikeMax.accept(mRide);
+            bikeMax.accept(tRide);
 
-        assertEquals(bikeMax.getCountForType(BicycleType.MountainBike), 1);
-        assertEquals(bikeMax.getCountForType(BicycleType.RoadBike), 2);
-        assertEquals(bikeMax.getCountForType(BicycleType.Tandem), 1);
-        assertEquals(bikeMax.getEnteredCount(), 4);
+            assertEquals(bikeMax.getCountForType(BicycleType.MountainBike), 1);
+            assertEquals(bikeMax.getCountForType(BicycleType.RoadBike), 2);
+            assertEquals(bikeMax.getCountForType(BicycleType.Tandem), 1);
+        }
 
-    }
+        @Test
+        public void EnteredCount(){
+            FunRide bikeMax = new FunRide(4);
+            BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3, BicycleType.MountainBike);
+            BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4, BicycleType.RoadBike);
+            BicycleSpecification tandemspec = new BicycleSpecification(12, 7, BicycleType.Tandem);
+
+
+            Bicycle rBike1 = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle rBike = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle mRide = new bicycles.specifications.BicycleFromSpec(mountainBikeSpec);
+            Bicycle tRide = new bicycles.specifications.BicycleFromSpec(tandemspec);
+
+            bikeMax.accept(rBike);
+            bikeMax.accept(mRide);
+            bikeMax.accept(tRide);
+
+            assertEquals(bikeMax.getEnteredCount(), 3);
+        }
+
+        @Test
+        public void Accept(){
+            FunRide bikeMax = new FunRide(3);
+            BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3, BicycleType.MountainBike);
+            BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4, BicycleType.RoadBike);
+            BicycleSpecification tandemspec = new BicycleSpecification(12, 7, BicycleType.Tandem);
+
+
+            Bicycle rBike1 = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle rBike = new bicycles.specifications.BicycleFromSpec(roadBikeSpec);
+            Bicycle mRide = new bicycles.specifications.BicycleFromSpec(mountainBikeSpec);
+            Bicycle tRide = new bicycles.specifications.BicycleFromSpec(tandemspec);
+
+            bikeMax.accept(rBike);
+            bikeMax.accept(rBike1);
+            bikeMax.accept(tRide);
+            bikeMax.accept(mRide);
+
+
+            assertEquals(bikeMax.getEnteredCount(), 3);
+        }
 
 }
